aa3ce6fc75bf4178e6716aaa763e96a90fad4624
Uriel <mamaniuriel592@gmail.com>
2026-05-04 08:21:30 -0300

    .

---
+++ EJERCICIO 1-TP3/src/ejercicio/pkg1/tp3/EJERCICIO1TP3.java
@@ -0,0 +1,50 @@
+
+package ejercicio.pkg1.tp3;
+
+public class EJERCICIO1TP3 {
+
+
+    public static void main(String[] args) {
+  
+
+//FOR
+              
+        for (int i = 1; i <= 100; i++) {
+            System.out.println("Numbero: " + i);
+        }
+
+
+//WHILE
+        
+        int numero = 1; 
+
+        while (numero<= 100) { 
+            System.out.println("Número: " + numero);
+            numero++; 
+        }
+
+
+//DO WHILE
+                
+           int count = 1; 
+
+        do {
+
+            System.out.println("Número: " + count);
+            count++; 
+        } while (count <= 100); 
+
+   
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+    }
+    
+}
