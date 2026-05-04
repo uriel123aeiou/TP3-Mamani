964f755ccebb5f18af3b5f28834b0b027dce6b30
Uriel <mamaniuriel592@gmail.com>
2026-05-04 08:43:25 -0300

    .

---
+++ EJERCICIO 3-TP3/src/ejercicio/pkg3/tp3/EJERCICIO3TP3.java
@@ -0,0 +1,33 @@
+
+package ejercicio.pkg3.tp3;
+
+import java.util.Scanner;
+
+
+public class EJERCICIO3TP3 {
+
+    
+    public static void main(String[] args) {
+     
+        
+        
+              Scanner scanner = new Scanner(System.in);
+
+        System.out.print("introduce un numero entero: ");
+
+        int N = scanner.nextInt();
+
+        
+            for (int i = 1; N >= i; N--) {
+                System.out.println(N); 
+            }
+
+        
+        
+        
+        
+        
+        
+    }
+    
+}
